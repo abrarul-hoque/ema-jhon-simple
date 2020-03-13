77036a703e0de354d638c1c2df8bd94bb8633936
Abrarul Hoque <jubairctg99@gmail.com>
2020-03-13 23:06:21 +0600

    Finished ema-jhon-simple coding

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart);
+    //const total = cart.reduce((total, prd) => total + prd.price, 0)
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = (total + product.price);
+    }
+    let shipping = 0;
+    if (total > 80) {
+        shipping = 0;
+    }
+    else if (total > 25) {
+        shipping = 4.99;
+    }
+    else if (total > 0) {
+        shipping = 12.11;
+    }
+
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
+    const formatNumber = num =>{
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <h5>Items Ordered: {cart.length} </h5>
+            <p>Total Price : ${formatNumber(total)}</p>
+            <p><small>Shipping Cost : ${shipping}</small></p>
+            <p><small>Tax + Vat : ${tax}</small></p>
+            <h5>Total Price : ${grandTotal}</h5>
+        </div>
+    );
+};
+
+export default Cart;
